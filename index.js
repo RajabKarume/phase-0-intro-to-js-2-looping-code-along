59a6d62910f93b15a31fc423cde0303f4bf9bbf9
Rajab <rkarume@gmail.com>
2022-08-22 06:53:14 +0300

    commit tree c31422fe62b71c2e5fc209b9236acc60369db04f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const writeCards= function(array){
+  let names = ["Guadalupe", "Ollie", "Aki"]
+  for (let item of array){
+    return item
+  }
+  return `Thank you ${name} for the surprise gift`
+}
+// function writeCards(fun){
+//   let names = ["Guadalupe", "Ollie", "Aki"];
+//   fun (names);
+  
+// }
